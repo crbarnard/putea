daa7cc0c8ddc42396ff56142a46e8e7cc54e6dac
Corey <8599356+crbarnard@users.noreply.github.com>
2026-08-08 14:38:27 +1200

    Add create user endpoint

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,6 +1,7 @@
 // src/users/users.service.ts
 import { Injectable } from '@nestjs/common';
 import { DrizzleService } from '../drizzle/drizzle.service';
+import * as schema from '../drizzle/schema';
 
 @Injectable()
 export class UsersService {
@@ -8,5 +9,9 @@ export class UsersService {
 
   async findAll() {
     return this.drizzle.db.query.users.findMany();
+  }
+
+  async createUser(user: typeof schema.users.$inferInsert) {
+    await this.drizzle.db.insert(schema.users).values(user);
   }
 }
